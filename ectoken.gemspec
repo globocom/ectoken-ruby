9f235f5fe797a20361a686e1093722c2621de891
Maurício Antunes <mauricio.abreua@gmail.com>
2020-08-07 11:30:31 -0300

    Add console/setup and refactor CLI

--- ectoken.gemspec
+++ ectoken.gemspec
@@ -1,5 +1,6 @@
 lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require "ectoken/version"
 
 Gem::Specification.new do |spec|
   spec.name = "ectoken"
@@ -23,5 +24,11 @@ Gem::Specification.new do |spec|
           "public gem pushes."
   end
 
+  spec.bindir = "exe"
+  spec.executables = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", "~> 2.0"
+  spec.add_development_dependency "rake", "~> 13.0"
+  spec.add_development_dependency "rspec", "~> 3.0"
 end
--- lib/ectoken/cli.rb
+++ lib/ectoken/cli.rb
@@ -1,29 +1,35 @@
-@options = {}
+module EdgeCastToken
+  class CLI
+    self.main()
+      @options = {}
 
-option_parser = OptionParser.new do |opts|
-  opts.on('-k', '--key=KEY', 'Key to encrypt or decrypt') do |v|
-    @options[:key] = v
+      option_parser = OptionParser.new do |opts|
+        opts.on('-k', '--key=KEY', 'Key to encrypt or decrypt') do |v|
+          @options[:key] = v
+        end
+
+        opts.on('-t', '--token=TOKEN', 'Token to encrypt or decrypt') do |v|
+          @options[:token] = v
+        end
+
+        opts.on('-d', '--decrypt', 'Use this option to decrypt data') do |v|
+          @options[:decrypt] = true
+        end
+      end
+
+      option_parser.parse!
+
+      if @options[:key].nil? || @options[:token].nil?
+        puts option_parser.help
+        exit 1
+      end
+
+      case @options[:decrypt].nil?
+      when true
+        puts encrypt(@options[:key], @options[:token])
+      when false
+        puts decrypt(@options[:key], @options[:token])
+      end
+    end
   end
-
-  opts.on('-t', '--token=TOKEN', 'Token to encrypt or decrypt') do |v|
-    @options[:token] = v
-  end
-
-  opts.on('-d', '--decrypt', 'Use this option to decrypt data') do |v|
-    @options[:decrypt] = true
-  end
-end
-
-option_parser.parse!
-
-if @options[:key].nil? || @options[:token].nil?
-  puts option_parser.help
-  exit 1
-end
-
-case @options[:decrypt].nil?
-when true
-  puts encrypt(@options[:key], @options[:token])
-when false
-  puts decrypt(@options[:key], @options[:token])
 end
